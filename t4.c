b3a89b9286b0271bda0aa94fba0c0f1ba0a7b0e1
nn80nn <nn80nn80nn80nn80nn@gmail.com>
2024-02-15 19:49:09 +0300

    pc4

--- t4.c
+++ t4.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
+#include <complex.h>
+
+
+
+typedef struct qmatrix
+{
+    void (*trancpon)(struct qmatrix*, void* data);
+    void (*multmat)(struct qmatrix*, void* data);
+    void (*multline)(struct qmatrix*, void* data);
+    void (*opred)(struct qmatrix*, void* data);
+} qmatrix;
 
 
 int main(void) {
