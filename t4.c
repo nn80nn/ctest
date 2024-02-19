f2bc61d7ebf9c8ce4342fd0bf4d4b3edcb7f36c5
nn80nn <nn80nn80nn80nn80nn@gmail.com>
2024-02-19 13:12:10 +0300

    pc6

--- t4.c
+++ t4.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <complex.h>
-
+#include  <stdlib.h>
 
 
 typedef struct qmatrix
 {
+    void (*create)(struct qmatrix*, void* data);
+    void (*show)(struct qmatrix*, void* data);
     void (*trancpon)(struct qmatrix*, void* data);
     void (*multmat)(struct qmatrix*, void* data);
     void (*multline)(struct qmatrix*, void* data);
@@ -24,7 +26,28 @@ typedef struct cplx{
       int: "int", \
       char: "char", \
       double: "double", \
-      default: "complex")
+      default: "default)
+
+
+void create() {
+    int n;
+    scanf(&n);
+    pa = (double *) malloc((n*n) * sizeof (double));
+    if (pa == NULL) { /*Проверка успешности выделения памяти.*/
+        printf("Не удалось выделить память.");
+        exit(EXIT_FAILURE); /*Аварийное завершение программы.*/
+    };
+    for (i = 0; i < n; i++)
+    {
+       for (j = 0; j < n; j++)
+       {
+        scanf(pa[i,j])
+       }
+       
+    }
+    
+
+}
 
 
 int main(void) {
